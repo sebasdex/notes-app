58c7cf88e083f6cf9c4989a428dda4c8a66bf764
dexterdc <sebasdc9@hotmail.com>
2025-01-21 11:00:21 -0600

    feat: add login and register forms

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,34 +3,34 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Login",
-    description: "A simple note app built with Next.js",
+  title: "Login",
+  description: "A simple note app built with Next.js",
 };
 
 export default function LoginLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <div className="bg-slate-900 min-h-screen flex flex-col items-center justify-center">
-                    <main>{children}</main>
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="bg-gray-300 min-h-screen flex flex-col items-center justify-center">
+          <main className="w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+"use client";
+import LoginForm from "@/components/LoginForm";
+import RegisterForm from "@/components/RegisterForm";
+import { useState } from "react";
 
 function page() {
-    return (
-        <section>
-            <form action="" className='flex flex-col gap-4'>
-                <h1 className='text-2xl font-bold text-slate-200'>Login</h1>
-                <input type="text" placeholder="Username" className='px-4 py-2 rounded-md border-2 border-slate-500 focus:border-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400' />
-                <input type="password" placeholder="Password" className='px-4 py-2 rounded-md border-2 border-slate-500 focus:border-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400' />
-                <button type="submit" className='px-4 py-2 rounded-md bg-slate-500 text-white hover:bg-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400'>Login</button>
-            </form>
-        </section>
-    )
+  const [isRegister, setIsRegister] = useState<boolean>(false);
+  return (
+    <>
+      {isRegister ? (
+        <RegisterForm setIsRegister={setIsRegister} />
+      ) : (
+        <LoginForm setIsRegister={setIsRegister} />
+      )}
+    </>
+  );
 }
 
-export default page
+export default page;
