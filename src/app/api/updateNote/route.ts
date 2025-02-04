a09caa5ad620b7be08a92d20b6af32fe7b067f76
dexterdc <sebasdc9@hotmail.com>
2025-02-04 17:17:15 -0600

    feat: Update note update functionality to include check-in and archive statuses

--- src/app/api/updateNote/route.ts
+++ src/app/api/updateNote/route.ts
@@ -4,14 +4,19 @@ import { createClient } from "@/config/supabaseServer";
 interface Note {
   id: string;
   textNote: string;
-  isDone: boolean;
+  isProtected: boolean;
+  isArchived: boolean;
 }
 
 export async function POST(req: Request) {
   try {
-    const { id, textNote, isDone } = await req.json();
-
-    if (!id || (textNote === undefined && isDone === undefined)) {
+    const { id, textNote, isProtected, isArchived } = await req.json();
+    if (
+      !id ||
+      (textNote === undefined &&
+        isProtected === undefined &&
+        isArchived === undefined)
+    ) {
       return NextResponse.json(
         { error: "Faltan datos: id y al menos un campo a actualizar" },
         { status: 400 }
@@ -22,7 +27,8 @@ export async function POST(req: Request) {
     const updateData: Partial<Note> = {};
 
     if (textNote !== undefined) updateData.textNote = textNote;
-    if (isDone !== undefined) updateData.isDone = isDone;
+    if (isProtected !== undefined) updateData.isProtected = isProtected;
+    if (isArchived !== undefined) updateData.isArchived = isArchived;
 
     const { data, error } = await supabase
       .from("newNotes")
@@ -34,7 +40,7 @@ export async function POST(req: Request) {
     }
 
     return NextResponse.json(
-      { message: "Nota actualizada correctamente", data },
+      { message: "Nota actualizada correctamente", data: updateData },
       { status: 200 }
     );
   } catch (error) {
