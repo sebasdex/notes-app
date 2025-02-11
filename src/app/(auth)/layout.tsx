b2c629ea18e83fa23a93f0b2fe835de14f157652
dexterdc <sebasdc9@hotmail.com>
2025-02-10 18:24:06 -0600

    feat: Refactor layout components to remove unused imports and simplify structure

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,40 +1,14 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import { Toaster } from "sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Login",
-  description: "A simple note app built with Next.js",
-};
 
 export default function LoginLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="bg-gray-300 min-h-screen flex flex-col items-center justify-center">
-          <main className="w-full">
-            {children}
-            <Toaster richColors position="top-center" />
-          </main>
-        </div>
-      </body>
-    </html>
+    <div className="bg-gray-300 min-h-screen flex flex-col items-center justify-center">
+      <main className="w-full">
+        {children}
+        <Toaster richColors position="top-center" />
+      </main>
+    </div>
   );
 }
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,45 +1,19 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import Nav from "@/components/Nav";
 import SideBar from "@/components/SideBar";
 import { NoteAppProvider } from "@/context/ContextNoteApp";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Note App",
-  description: "A simple note app built with Next.js",
-};
-
-export default async function RootLayout({
+export default function HomeLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NoteAppProvider>
-          <div className="grid-area-container">
-            <Nav />
-            <SideBar />
-            <main className="main">
-              {children}
-            </main>
-          </div>
-        </NoteAppProvider>
-      </body>
-    </html>
+    <NoteAppProvider>
+      <div className="grid-area-container">
+        <Nav />
+        <SideBar />
+        <main className="main">{children}</main>
+      </div>
+    </NoteAppProvider>
   );
 }
